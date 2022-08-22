7c9e30f82968fc9f4ec075aaa7d875a65ddce511
utquarsh027 <87616610+utquarsh027@users.noreply.github.com>
2022-08-22 14:04:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -17,17 +17,17 @@ if url!="":
     video=yt.streams
     if len(video)>0:
         downloaded,download_audio=False,False
-        st.text("Enter a destination on your system to download")
-        destination1=st.text_input(label="",placeholder="Directory/Location on your computer")
+#         st.text("Enter a destination on your system to download")
+#         destination1=st.text_input(label="",placeholder="Directory/Location on your computer")
         download_video = st.button("Download Video")
         if yt.streams.filter(only_audio=True):
             download_audio = st.button("Download Audio Only")
         if download_video:
-            video.get_highest_resolution().download(output_path=destination1)
+            video.get_highest_resolution().download()
             downloaded = True    
             
         if download_audio:
-            video.filter(only_audio=True).first().download(output_path=destination1)
+            video.filter(only_audio=True).first().download()
             downloaded = True
         if downloaded:
             #result
